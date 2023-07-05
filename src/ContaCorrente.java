55c61b0d2eae1bc4e7aa687dad46a3a10884428d
brunooSilva13 <brunomendes.2259@gmail.com>
2023-07-05 09:10:11 -0300

    Initial commit

---
+++ src/ContaCorrente.java
@@ -0,0 +1,23 @@
+public class ContaCorrente extends Conta implements Tributavel {
+
+
+    public ContaCorrente(int agencia, int numero) {
+        super(agencia, numero);
+    }
+
+    @Override
+    public void deposita(double valor) {
+        super.saldo += valor;
+    }
+
+    @Override //anotação na configuração do codigo java
+    public boolean saca(double valor) {
+        double valorASacar = valor + 0.2;
+        return super.saca(valorASacar);
+    }
+
+    @Override
+    public double getValorImposto() {
+        return super.saldo * 0.01;
+    }
+}
